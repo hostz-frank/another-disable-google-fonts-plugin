01f419f7fb9564d3f685d44b85d5947f532d23f8
Frank Stuerzebecher <frank@hostz.at>
2015-01-13 13:07:25 +0100

    Add disabling external font load for theme Twenty Fifteen (WP v4.1)

--- another-disable-google-fonts-plugin.php
+++ another-disable-google-fonts-plugin.php
@@ -1,27 +1,40 @@
 <?php
 /**
- * Another Disable Google Fonts Plugin
- *
- * Disable enqueuing of Open Sans and other fonts used by WordPress from Google.
- */
-
-/**
  * Plugin Name: Another Disable Google Fonts Plugin
  * Description: Disable Open Sans and other fonts fetched from Google by WordPress core.
  * Author: Frank St&uuml;rzebecher
  * Author URI: http://netzklad.de/
- * Version: 0.1
- * License: GPL
+ * Version: 0.2
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not, write
+ * to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
+
 /* Exit if accessed directly */
-if ( ! defined( 'ABSPATH' ) ) exit;
+defined( 'ABSPATH' ) || die();
 
 
 function google_fonts_load_disable( $styles ) {
-	$styles->add( 'open-sans', '' ); // Backend
-	$styles->add( 'twentyfourteen-lato', '' ); // Themes ...
+	$styles->add( 'open-sans'           , '' ); // Backend
+	$styles->add( 'twentytwelve-fonts'  , '' ); // Core themes ...
 	$styles->add( 'twentythirteen-fonts', '' );
-	$styles->add( 'twentytwelve-fonts', '' );
+	$styles->add( 'twentyfourteen-lato' , '' );
+	$styles->add( 'twentyfifteen-fonts',  '' );
+
+	if ( is_admin() && function_exists( 'twentyfifteen_fonts_url' ) ) {
+		global $editor_styles;
+		unset( $editor_styles[ array_search( twentyfifteen_fonts_url(), $editor_styles ) ] );
+	}
 }
+
 add_action( 'wp_default_styles', 'google_fonts_load_disable', 5 );
